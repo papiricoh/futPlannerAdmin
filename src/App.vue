e71142724e189cb0a23fca5fd6f49ed5c928e481
papiricoh <slendeman0200@gmail.com>
2023-11-20 02:30:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<script>
+  export default {
+    name: 'Main',
+    data() {
+      return {
+        
+        titulo: 'Hola, Vue!'
+      }
+    },
+    methods: {
+      
+    },
+    computed: {
+      
+    },
+  };
+</script>
+
+<template>
+  <header>
+    <div>USERNDWJADAADJWJAJDIWIAJME</div>
+  </header>
+
+  <RouterView  class="readerPage" />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  width: min-content;
+  width: 100%;
+  overflow: auto;
+  word-break: break-all;
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        
+        titulo: 'Hola, Vue!'
+      }
+    },
+    methods: {
+      
+    },
+    computed: {
+      
+    },
+  };
+</script>
+
+<template>
+  <div class="home">
+    <div>{{titulo}}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
