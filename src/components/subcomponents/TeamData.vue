80985ed9ba30ee8c1793ee1afa3044bbb4130b0b
papiricoh <slendeman0200@gmail.com>
2023-12-04 14:03:24 +0100

    added structure to teamData

--- src/components/subcomponents/TeamData.vue
+++ src/components/subcomponents/TeamData.vue
@@ -44,15 +44,43 @@ import LoadingBall from '../loading/LoadingBall.vue';
     
   </div>
   <div class="data_container" v-else>
+    <div class="data_row">
+      <div class="data_box">
+        
+      </div>
+      <div class="data_box">
 
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.data_container {
-  background-color: black;
-  height: 4rem;
+.data_box {
+  min-height: 4rem;
   width: 100%;
+  border-radius: .2rem;
+  padding: .4rem;
+  background-color: rgba(128, 128, 128, 0.2);
+}
+.data_row {
+  width: 100%;
+  box-sizing: border-box;
+  grid: 1rem;
+  min-height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: stretch;
+  gap: 1rem;
+}
+.data_container {
+  min-height: 4rem;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  box-sizing: border-box;
 }
 .loading_padding {
   padding: 1rem;
