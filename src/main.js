27903cc3fcea44cea6a6d844b430c9a328583e54
Papiricoh <slendeman0200@gmail.com>
2023-12-27 02:02:58 +0100

    added fontAwesome

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store.js';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
 
 const app = createApp(App)
 
 app.use(store);
 app.use(router)
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app')
