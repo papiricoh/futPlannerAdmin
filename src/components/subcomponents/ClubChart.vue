3ee4f79aac1f510a6ab37c9d99873a056823f3f1
Papiricoh <slendeman0200@gmail.com>
2024-05-12 03:08:47 +0200

    added final chart

--- src/components/subcomponents/ClubChart.vue
+++ src/components/subcomponents/ClubChart.vue
@@ -17,29 +17,41 @@ export default {
         loading: true,
         
         chartOptions: {
-            chart: {
-                type: 'column'
-            },
+          chart: {
+            type: 'column'
+          },
+          title: {
+            text: 'Vision general'
+          },
+          xAxis: {
+            categories: []
+          },
+          yAxis: {
             title: {
-                text: 'Vision general'
-            },
-            xAxis: {
-                categories: []
-            },
-            yAxis: {
-                title: {
-                    text: 'Media'
-                }
-            },
-            series: [
-                /*{
-                name: 'Tokio',
-                data: [4]
-                }, {
-                name: 'Nueva York',
-                data: [42]
-                }*/
-            ]
+              text: 'Media'
+            }
+          },
+          series: [
+            {
+              name: 'Rendimiento General',
+              data: []
+            }, {
+              name: 'Rendimiento Tactico',
+              data: []
+            }, {
+              name: 'Calidad de pases',
+              data: []
+            }, {
+              name: 'Control de balón',
+              data: []
+            }, {
+              name: 'Vision de juego',
+              data: []
+            }, {
+              name: 'Tiempo medio de juego',
+              data: []
+            }
+          ]
         }
       }
     },
@@ -47,12 +59,53 @@ export default {
         
     },
     methods: {
+      async getClubAnalytics() {
+        if(this.text_edit_input == "") {
+          return;
+        }
+        this.loading = true;
+        const postData = {
+          user_id: this.getUser.id,
+          token: this.getUser.last_token_key,
+        };
+        try {
+          const res = await fetch(`${this.$store.getters.getBaseURL}/getClubAnalytics/owner`, {
+            method: "post",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify(postData),
+          });
+          if (!res.ok) {
+            throw new Error(`An error has occurred: ${res.status} - ${res.statusText}`);
+          }
+          const data = await res.json();
+
+          for (const team of data) {
+            if(team.total_reports != 0) {
+              this.chartOptions.xAxis.categories.push(team.team_name)
+              
+              this.chartOptions.series[0].data.push(Number(team.avg_general_performance))
+              this.chartOptions.series[1].data.push(Number(team.avg_tactical_performance))
+              this.chartOptions.series[2].data.push(Number(team.avg_passes_quality))
+              this.chartOptions.series[3].data.push(Number(team.avg_ball_control))
+              this.chartOptions.series[4].data.push(Number(team.avg_game_vision))
+              this.chartOptions.series[5].data.push(Number(team.avg_played_time))
+            }
+          }
+
+          this.loading = false;
+
+        } catch (err) {
+          //TODO: this.generalError.error = "No conexion error: " + err.message;
+        }
+      },
       async checkUserLoaded() {
         const intervalId = await setInterval(async () => {
           if (this.getUser.id != 0) {
             clearInterval(intervalId);
             
-            this.loading = false
+            await this.getClubAnalytics()
           }
         }, 400);
       },
